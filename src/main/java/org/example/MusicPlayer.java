61a2d3b065a27fc3b674d5b3006013e9365aaaa3
Stepan <stsalamtat@gmail.com>
2023-03-15 18:27:58 +0300

    creating multiple dependencies

---
+++ src/main/java/org/example/MusicPlayer.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class MusicPlayer {
+    private final Music music;
+    //IoC
+    public MusicPlayer(Music music){
+        this.music = music;
+    }
+    public void playMusic(){
+        System.out.println("Playing: " + music.getSong());
+    }
+
+}
