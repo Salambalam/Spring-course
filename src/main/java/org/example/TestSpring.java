8ce99d674fbd2e950c836225bbf6413cc0d21232
Stepan <stsalamtat@gmail.com>
2023-03-18 17:20:39 +0300

    DI annotation authowired

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,12 +7,16 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("rockMusic", Music.class);
-        Music music1 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer player = new MusicPlayer(music);
-        MusicPlayer player1 = new MusicPlayer(music1);
-        player.playMusic();
-        player1.playMusic();
+//        Music music = context.getBean("rockMusic", Music.class);
+//        Music music1 = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer player = new MusicPlayer(music);
+//        MusicPlayer player1 = new MusicPlayer(music1);
+//        player.playMusic();
+//        player1.playMusic();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
