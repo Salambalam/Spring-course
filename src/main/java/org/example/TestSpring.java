d52e0b4b99fbd6021dad52f330a33e7d72dbc8bd
Stepan <stsalamtat@gmail.com>
2023-03-18 16:49:29 +0300

    annotation introduction, annotation Component

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,9 +7,12 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("musicBean", Music.class);
+        Music music = context.getBean("rockMusic", Music.class);
+        Music music1 = context.getBean("classicalMusic", Music.class);
         MusicPlayer player = new MusicPlayer(music);
+        MusicPlayer player1 = new MusicPlayer(music1);
         player.playMusic();
+        player1.playMusic();
         context.close();
     }
 }
