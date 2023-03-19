bce9ba675079e0e31b2fd29f4186106244695e39
Stepan <stsalamtat@gmail.com>
2023-03-19 19:01:58 +0300

    full Spring java cofig

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -1,22 +1,13 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                SpringConfig.class
         );
-//        Music music = context.getBean("rockMusic", Music.class);
-//        Music music1 = context.getBean("classicalMusic", Music.class);
-//        MusicPlayer player = new MusicPlayer(music);
-//        MusicPlayer player1 = new MusicPlayer(music1);
-//        player.playMusic();
-//        player1.playMusic();
-//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        musicPlayer.playMusic();
-//        Computer computer = context.getBean("computer", Computer.class);
-//        System.out.println(computer);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
